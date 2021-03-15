63b329aebd7b73d29003bcde0452d2f7fcc5dde8
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2021-03-15 20:15:45 -0300

    chore(database): connection created and cli setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [
+    ConfigModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      // migrations: ['./migrations/*.ts'],
+      // cli: {
+      //   entitiesDir: `${__dirname} /../**/*.entity{.ts,.js}`,
+      //   migrationsDir: './migrations'
+      // },
+      synchronize: true // shouldn't be used in production
+    }),
+    UserModule
+  ]
 })
 export class AppModule {}
---
+++ src/migrations/1615849551139-CreateUsersTable.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class CreateUsersTable1615849551139 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
